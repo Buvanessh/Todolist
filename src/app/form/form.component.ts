bd542f244d1dd4fa7c6dba9d3280ed4587dd207f
Buvanessh <buvanessh111@gmail.com>
2025-05-05 22:34:56 +0530

    angular todolist task

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  list:String="";
+  lists:String[]=[];
+  addlist()
+  {
+    this.lists.push(this.list);
+    this.list="";
+  }
+
+  deleteall()
+  {
+    this.lists=[]
+  }
+
+ 
+}
